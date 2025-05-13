a64d6a76c34a21fa21c4f22063b6c9db265f1355
pranjay-t <pranjayt90@gmail.com>
2025-05-13 10:54:59 +0530

    Flutter Project created

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_task_catalift/task_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter-Task-Catalift',
+      theme: ThemeData.light(useMaterial3: true),
+      home: const TaskScreen(),
+    );
+  }
+}
